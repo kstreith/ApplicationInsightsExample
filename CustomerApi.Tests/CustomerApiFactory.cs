8206dcad1938311b76350fbe4e27cd2170111a59
Kip <kstreith@yahoo.com>
2019-09-22 19:57:52 -0400

    Fix unit tests to always use InMemory storage

--- CustomerApi.Tests/CustomerApiFactory.cs
+++ CustomerApi.Tests/CustomerApiFactory.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace CustomerApi.Tests
 {
     public class CustomerApiFactory : WebApplicationFactory<Startup>
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var dict = new Dictionary<string, string>
+                {
+                    ["Storage"] = "InMemory"
+                };
+                config.AddInMemoryCollection(dict);
+            });
+            base.ConfigureWebHost(builder);
+        }
         protected override TestServer CreateServer(IWebHostBuilder builder)
         {
             var server = base.CreateServer(builder);
