e0c463ed2d7e7b0b0ca41f818aef8af004a4ecc0
Streithorst <kip.streithorst@appliedis.com>
2019-09-30 22:07:05 -0400

    Add user logging, clean up config and add guid scrubbing

--- CustomerApi/ApplicationInsights/ScrubCustomerIdTelemetryProcessor.cs
+++ CustomerApi/ApplicationInsights/ScrubCustomerIdTelemetryProcessor.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Text.RegularExpressions;
 
 namespace CustomerApi
 {
-    public class CustomTelemetryProcessor : ITelemetryProcessor
+    public class ScrubCustomerIdTelemetryProcessor : ITelemetryProcessor
     {
         private readonly ITelemetryProcessor _next;
+        private readonly Regex _guidRegex;
 
-        public CustomTelemetryProcessor(ITelemetryProcessor next)
+        public ScrubCustomerIdTelemetryProcessor(ITelemetryProcessor next)
         {
             _next = next;
+            _guidRegex = new Regex("([0-9A-Fa-f]{8}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{4}[-][0-9A-Fa-f]{12})");
         }
 
         public void Process(ITelemetry item)
@@ -20,12 +24,7 @@ namespace CustomerApi
                 var request = item as RequestTelemetry;
                 if (request != null)
                 {
-                    request.Properties["wentThroughCustomProcessor"] = "true";
-                }
-                var trace = item as TraceTelemetry;
-                if (trace != null)
-                {
-                    trace.Properties["customProp"] = "customValue";
+                    request.Url = new Uri(_guidRegex.Replace(request.Url.AbsoluteUri, "guid"));
                 }
             }
             finally
--- CustomerApi/Config/DependencyExtensions.cs
+++ CustomerApi/Config/DependencyExtensions.cs
@@ -1,13 +1,30 @@
 ﻿using CustomerApi.Business.Services.Customer;
 using CustomerApi.Business.Settings;
+using DataRepository.Cosmos;
+using DataRepository.InMemory;
+using DataRepository.SqlServer;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace CustomerApi.Config
 {
     public static class DependencyExtensions
     {
-        public static void RegisterAppDependencies(this IServiceCollection services)
+        public static void RegisterAppDependencies(this IServiceCollection services, IConfiguration config)
         {
+            if (string.Equals(config["Storage"], "Cosmos", StringComparison.InvariantCultureIgnoreCase))
+            {
+                services.RegisterCosmosDependencies(config);
+            }
+            else if (string.Equals(config["Storage"], "SqlServer", StringComparison.InvariantCultureIgnoreCase))
+            {
+                services.RegisterSqlServerDependencies(config);
+            }
+            else
+            {
+                services.RegisterInMemoryDependencies(config);
+            }
             services.AddTransient<CreateSampleCustomerDataService, CreateSampleCustomerDataService>();
             services.AddTransient<CreateCustomerService, CreateCustomerService>();
             services.AddTransient<GetCustomerService, GetCustomerService>();
