0d411a66a875140ec4a6ad2b734e52a5d7851ee9
Streithorst <kip.streithorst@appliedis.com>
2020-03-02 08:37:01 -0500

    Add AppInsights API authentication key

--- CustomerApi/Config/ApplicationInsightsConfigExtensions.cs
+++ CustomerApi/Config/ApplicationInsightsConfigExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -19,6 +20,10 @@ namespace CustomerApi.Config
                 config.Bind("ApplicationInsights", options);
             });
             services.AddApplicationInsightsTelemetryProcessor<ScrubCustomerIdTelemetryProcessor>();
+            services.ConfigureTelemetryModule<QuickPulseTelemetryModule>((module, o) =>
+            {
+                module.AuthenticationApiKey = config["ApplicationInsights:AuthenticationApiKey"];
+            });
             services.AddSingleton<ITelemetryInitializer, FakeApiUserTelemetryInitializer>();
             services.PostConfigureAll<LoggerFilterOptions>(action =>
             {
